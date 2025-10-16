9e703c38a619217ba82135b26ef1f876167e2566
IcaroAS-20 <icaroalvessilva84@gmail.com>
2025-10-16 15:03:04 -0300

    Add files via upload

---
+++ Estudo/scriptEs.js
@@ -0,0 +1,15 @@
+function mostrarMT() {
+   window.location.href = "Matemática/indexMT.html";
+}
+function mostrarLP() {
+    window.location.href = "LinguaPortuguesa/indexLP.html";
+}
+function mostrarCSH() {
+    window.location.href = "CSH/indexCSH.html";
+}
+function mostrarCSN() {
+    window.location.href = "CSN/indexCSN.html";
+}
+function mostrarN() {
+    window.location.href = "../Navegador/indexN.html";
+}
